89a2969ee92ff1583325d4016b3560b2a025a005
keerthi <39428893+keerthi824@users.noreply.github.com>
2023-07-01 11:50:37 +0530

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -2,58 +2,41 @@ provider "azurerm" {
   features {}
 }
 
-data "external" "open_azure_portal" {
-
-  program = ["cmd.exe", "/C", "start https://portal.azure.com"]
-
- }
-
-
-resource "null_resource" "open_azure_portal" {
-
-  triggers = {
-
-    external_data = data.external.open_azure_portal.result
-
-  }
-
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
+resource "azurerm_resource_group" "resgrp" {
+  name     = "keerthi"
   location = "West US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "my-virtual-network"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "virtual-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.resgrp.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "my-network-interface"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "nic" {
+  name                = "network-interface"
+  location            = azurerm_resource_group.resgrp.location
+  resource_group_name = azurerm_resource_group.resgrp.name
 
   ip_configuration {
-    name                          = "my-ip-configuration"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "ip-config"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
-  name                  = "my-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm-001"
+  location              = azurerm_resource_group.resgrp.location
+  resource_group_name   = azurerm_resource_group.resgrp.name
   vm_size               = "Standard_DS2_v2"
   delete_os_disk_on_termination = true
   delete_data_disks_on_termination = true
@@ -63,7 +46,7 @@ resource "azurerm_virtual_machine" "example" {
     ScheduledStop  = "Yes"
   }
 
-  network_interface_ids = [azurerm_network_interface.example.id]
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   storage_image_reference {
     publisher = "Canonical"
@@ -73,20 +56,21 @@ resource "azurerm_virtual_machine" "example" {
   }
 
   storage_os_disk {
-    name              = "my-vm-osdisk"
+    name              = "osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "my-vm"
-    admin_username = "adminuser"
-
-    admin_password = "Password1234!"  # Note: Use a strong password and consider using secrets management systems instead
+    computer_name  = "vm-01"
+    admin_username = "keer"
+    admin_password = "azureuser@123"  # Note: Use a strong password and consider using secrets management systems instead
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 }
+
+
